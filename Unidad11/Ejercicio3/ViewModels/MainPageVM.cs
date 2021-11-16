f40831b1a8bf1c85bcb6b29b29007c7f1b94e986
Santi <santima1904@gmail.com>
2021-11-16 19:47:00 +0100

    command no

---
+++ Unidad11/Ejercicio3/ViewModels/MainPageVM.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unidad11.DAL;
+using Unidad11.Entidades;
+using Windows.UI.Xaml;
+using System.Collections.ObjectModel;
+using Ejercicio3_UI.Models.ViewModels;
+using _18_CRUD_Personas_UWP_UI.ViewModels.Utilidades;
+
+namespace Ejercicio3.ViewModels
+{
+    public class MainPageVM : clsVMBase
+    {
+        private ObservableCollection<clsPersona> listaPersonas;
+        private clsPersona opersonaSeleccionada;
+        private DelegateCommand borrarCommand;
+
+        #region constructores
+        public MainPageVM()
+        {
+            listaPersonas = new clsListado().rellenarLista();
+            opersonaSeleccionada = new clsPersona();
+        }
+
+        public ObservableCollection<clsPersona> ListaPersonas { get => listaPersonas; set => listaPersonas = value; }
+        public clsPersona OpersonaSeleccionada { get => opersonaSeleccionada; set => opersonaSeleccionada = value; }
+        public DelegateCommand BorrarCommand 
+        { 
+            get {
+                DelegateCommand delegateCommand = new DelegateCommand(BorrarCommand_Executed, PuedeEjercutar);
+                return borrarCommand; 
+            }
+            set => borrarCommand = value; 
+        }
+
+        #endregion
+
+        private void BorrarCommand_Executed()
+        {
+            listaPersonas.Remove(opersonaSeleccionada);
+            NotifyPropertyChanged("ListaPersonas");
+        }
+
+        private bool PuedeEjercutar()
+        {
+            bool valido = false;
+            if(opersonaSeleccionada != null)
+            {
+                valido = true;
+            }
+
+            return valido;
+        }
+    }
+}
