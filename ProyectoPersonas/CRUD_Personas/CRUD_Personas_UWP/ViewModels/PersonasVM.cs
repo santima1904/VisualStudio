1c495c83fa1f1e59a8b5c45c94af1fdec501b82b
Santi <santima1904@gmail.com>
2021-12-01 13:42:29 +0100

    UWP empezado

---
+++ ProyectoPersonas/CRUD_Personas/CRUD_Personas_UWP/ViewModels/PersonasVM.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CRUD_Personas_Entidades;
+using CRUD_Personas_BL.Gestora;
+using CRUD_Personas_BL.Listado;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace CRUD_Personas_UWP.ViewModels
+{
+    public class PersonasVM : INotifyPropertyChanged
+    {
+        ObservableCollection<clsPersona> listadoPersonasCompleto;
+        ObservableCollection<Departamento> listadoDepartamentosCompleto;
+        clsPersona personaSeleccionada;
+
+        public ObservableCollection<clsPersona> ListadoPersonasCompleto { get => listadoPersonasCompleto; set => listadoPersonasCompleto = value; }
+        public ObservableCollection<Departamento> ListadoDepartamentosCompleto { get => listadoDepartamentosCompleto; set => listadoDepartamentosCompleto = value; }
+        public clsPersona PersonaSeleccionada { 
+            get => personaSeleccionada;
+            set
+            {
+                personaSeleccionada = value;
+                NotifyPropertyChanged("PersonaSeleccionada");
+            }
+            }
+
+    public PersonasVM()
+        {
+            this.listadoPersonasCompleto = clsListadoPersonasBL.obtenerListadoPersonasCompleto_BL();
+            this.listadoDepartamentosCompleto = clsDepartamentosBL.obtenerListadoDepartamentosCompleto_BL();
+            this.personaSeleccionada=new clsPersona();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(String nombre)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(nombre));
+            }
+        }
+    }
+}
