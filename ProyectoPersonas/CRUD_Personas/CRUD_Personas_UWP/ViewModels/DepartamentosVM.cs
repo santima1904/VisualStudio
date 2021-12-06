c968753a5825a2ba3a4679369a3e2e808cbe782d
Santi <santima1904@gmail.com>
2021-12-06 20:44:13 +0100

    uwp avanzado

---
+++ ProyectoPersonas/CRUD_Personas/CRUD_Personas_UWP/ViewModels/DepartamentosVM.cs
@@ -0,0 +1,83 @@
+﻿using CRUD_Personas_Entidades;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CRUD_Personas_BL.Listado;
+using CRUD_Personas_UWP.ViewModels.Utilidades;
+
+namespace CRUD_Personas_UWP.ViewModels
+{
+    public class DepartamentosVM : clsVMBase
+    {
+        ObservableCollection<Departamento> listadoDepartamentosCompleto;
+        ObservableCollection<Departamento> listadoDepartamentosOfrecido;
+        Departamento departamentoSeleccionado;
+        private DelegateCommand buscarCommand;
+        private string txbBarraBusqueda;
+
+        public ObservableCollection<Departamento> ListadoDepartamentosCompleto { get => listadoDepartamentosCompleto; set => listadoDepartamentosCompleto = value; }
+        public Departamento DepartamentoSeleccionado 
+        { 
+            get => departamentoSeleccionado;
+            set
+            {
+                departamentoSeleccionado = value;
+                NotifyPropertyChanged("DepartamentoSeleccionado");
+            }
+        }
+
+        public DelegateCommand BuscarCommand
+        {
+            get
+            {
+                buscarCommand = new DelegateCommand(BuscarCommand_Executed, PuedeEjercutarBuscarCommand);
+                return buscarCommand;
+            }
+            set => buscarCommand = value;
+        }
+
+        public string TxbBarraBusqueda
+        {
+            get => txbBarraBusqueda;
+            set
+            {
+                txbBarraBusqueda = value;
+                NotifyPropertyChanged("TxbBarraBusqueda");
+                buscarCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        public ObservableCollection<Departamento> ListadoDepartamentosOfrecido { get => listadoDepartamentosOfrecido; set => listadoDepartamentosOfrecido = value; }
+
+        public DepartamentosVM()
+        {
+            this.listadoDepartamentosCompleto = clsDepartamentosBL.obtenerListadoDepartamentosCompleto_BL();
+            this.listadoDepartamentosOfrecido = listadoDepartamentosCompleto;
+        }
+
+        private void BuscarCommand_Executed()
+        {
+            listadoDepartamentosOfrecido = new ObservableCollection<Departamento>(from d in listadoDepartamentosCompleto
+                                                                                  where d.Nombre.ToLower().Contains(txbBarraBusqueda.ToLower()) 
+                                                                                  select d);
+            NotifyPropertyChanged("ListadoDepartamentosOfrecido");
+        }
+
+        private bool PuedeEjercutarBuscarCommand()
+        {
+            bool valido = true;
+
+            if (String.IsNullOrEmpty(txbBarraBusqueda))//aprovecho que hago la comprobación de si es nulo el textbox para eso
+            {
+                valido = false;
+                listadoDepartamentosOfrecido = listadoDepartamentosCompleto;
+                NotifyPropertyChanged("ListadoDepartamentosOfrecido");
+            }
+            return valido;
+        }
+
+    }
+}
--- ProyectoPersonas/CRUD_Personas/CRUD_Personas_UWP/ViewModels/PersonasVM.cs
+++ ProyectoPersonas/CRUD_Personas/CRUD_Personas_UWP/ViewModels/PersonasVM.cs
@@ -8,42 +8,165 @@ using CRUD_Personas_BL.Gestora;
 using CRUD_Personas_BL.Listado;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using CRUD_Personas_UWP.Models;
+using CRUD_Personas_UWP.ViewModels.Utilidades;
 
 namespace CRUD_Personas_UWP.ViewModels
 {
-    public class PersonasVM : INotifyPropertyChanged
+    public class PersonasVM : clsVMBase
     {
         ObservableCollection<clsPersona> listadoPersonasCompleto;
+        ObservableCollection<clsPersona> listadoPersonasOfrecido;
         ObservableCollection<Departamento> listadoDepartamentosCompleto;
-        clsPersonaConNombreDepartamento personaSeleccionada;
+        clsPersona personaSeleccionada;
+        string nombreDepartamento;
+        private DelegateCommand buscarCommand;
+        private DelegateCommand anhadirCommand;
+        private DelegateCommand borrarCommand;
+        private DelegateCommand editarCommand;
+        private DelegateCommand guardarCommand;
+        private string txbBarraBusqueda;
 
         public ObservableCollection<clsPersona> ListadoPersonasCompleto { get => listadoPersonasCompleto; set => listadoPersonasCompleto = value; }
         public ObservableCollection<Departamento> ListadoDepartamentosCompleto { get => listadoDepartamentosCompleto; set => listadoDepartamentosCompleto = value; }
-        public clsPersonaConNombreDepartamento PersonaSeleccionada { 
+        public clsPersona PersonaSeleccionada { 
             get => personaSeleccionada;
             set
             {
                 personaSeleccionada = value;
+                if(personaSeleccionada != null)
+                {
+                    getNombreDepartamentoDePersona();
+                    NotifyPropertyChanged("NombreDepartamento");
+                }
                 NotifyPropertyChanged("PersonaSeleccionada");
             }
         }
-        public PersonasVM()
+
+        public string NombreDepartamento 
         {
-                this.listadoPersonasCompleto = clsListadoPersonasBL.obtenerListadoPersonasCompleto_BL();
-                this.listadoDepartamentosCompleto = clsDepartamentosBL.obtenerListadoDepartamentosCompleto_BL();
-                //this.personaSeleccionada=new clsPersona();
+            get { return nombreDepartamento; }
+            set { nombreDepartamento = value; }
         }
-
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        private void NotifyPropertyChanged(String nombre)
+        public DelegateCommand BuscarCommand 
         {
-            if (PropertyChanged != null)
+            get 
+            { 
+                buscarCommand = new DelegateCommand(BuscarCommand_Executed, PuedeEjercutarBuscarCommand);
+                return buscarCommand;
+            }
+            set => buscarCommand = value; 
+        }
+        public string TxbBarraBusqueda 
+        { 
+            get => txbBarraBusqueda;
+            set
             {
-                PropertyChanged(this, new PropertyChangedEventArgs(nombre));
+                txbBarraBusqueda = value;
+                NotifyPropertyChanged("TxbBarraBusqueda");
+                buscarCommand.RaiseCanExecuteChanged();
             }
         }
+        public ObservableCollection<clsPersona> ListadoPersonasOfrecido { get => listadoPersonasOfrecido; }
+        public DelegateCommand AnhadirCommand {
+            get
+            {
+                buscarCommand = new DelegateCommand(AnhadirCommand_Executed, PuedeEjercutarCommandBar);
+                return buscarCommand;
+            }
+            set => anhadirCommand = value; }
+        public DelegateCommand BorrarCommand {
+            get
+            {
+                buscarCommand = new DelegateCommand(BorrarCommand_Executed, PuedeEjercutarCommandBar);
+                return buscarCommand;
+            }
+            set => borrarCommand = value; 
+        }
+        public DelegateCommand EditarCommand {
+            get
+            {
+                buscarCommand = new DelegateCommand(EditarCommand_Executed, PuedeEjercutarCommandBar);
+                return buscarCommand;
+            }
+            set => editarCommand = value; 
+        }
+        public DelegateCommand GuardarCommand {
+            get
+            {
+                buscarCommand = new DelegateCommand(GuardarCommand_Executed, PuedeEjercutarCommandBar);
+                return buscarCommand;
+            }
+            set => guardarCommand = value; 
+        }
+
+        public PersonasVM()
+        {
+            this.listadoPersonasCompleto = clsListadoPersonasBL.obtenerListadoPersonasCompleto_BL();
+            this.listadoDepartamentosCompleto = clsDepartamentosBL.obtenerListadoDepartamentosCompleto_BL();
+            this.listadoPersonasOfrecido = listadoPersonasCompleto;
+        }
+
+        private void getNombreDepartamentoDePersona()
+        {
+            foreach(Departamento departamento in listadoDepartamentosCompleto)
+            {
+                if(departamento.Id == personaSeleccionada.IdDepartamento)
+                {
+                    NombreDepartamento = departamento.Nombre;
+                }
+            }
+        }
+
+        private void BuscarCommand_Executed()
+        {
+            listadoPersonasOfrecido = new ObservableCollection<clsPersona>(from p in listadoPersonasCompleto
+                                                                         where p.Nombre.ToLower().Contains(txbBarraBusqueda.ToLower()) || p.Apellidos.ToLower().Contains(txbBarraBusqueda.ToLower())
+                                                                         select p);
+            NotifyPropertyChanged("ListadoPersonasOfrecido");
+        }
+
+        private bool PuedeEjercutarBuscarCommand()
+        {
+            bool valido = true;
+
+            if (String.IsNullOrEmpty(txbBarraBusqueda))//aprovecho que hago la comprobación de si es nulo el textbox para eso
+            {
+                valido = false;
+                listadoPersonasOfrecido = listadoPersonasCompleto;
+                NotifyPropertyChanged("ListadoPersonasOfrecido");
+            }
+            return valido;
+        }
+
+        private bool PuedeEjercutarCommandBar()
+        {
+            bool valido = true;
+
+            if (personaSeleccionada == null)//aprovecho que hago la comprobación de si es nulo el textbox para eso
+            {
+                valido = false;
+            }
+            return valido;
+        }
+
+        private void AnhadirCommand_Executed()
+        {
+            
+        }
+        private void GuardarCommand_Executed()
+        {
+            
+        }
+
+        private void BorrarCommand_Executed()
+        {
+            
+        }
+
+        private void EditarCommand_Executed()
+        {
+            
+        }
+
     }
 }
