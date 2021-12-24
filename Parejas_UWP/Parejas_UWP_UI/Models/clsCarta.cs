d9b8341f51d23fe85113e8f2a2fc1f63878093c6
Santi <santima1904@gmail.com>
2021-12-24 19:35:32 +0100

    parejitas empezado

---
+++ Parejas_UWP/Parejas_UWP_UI/Models/clsCarta.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parejas_UWP_UI.Models
+{
+    public class clsCarta
+    {
+        #region atributos
+        string nombre;
+        string foto;
+        #endregion
+
+        #region constructor
+        public clsCarta(string nombre)
+        {
+            this.nombre = nombre;
+            this.foto = "//Assets/Images/logoSDLT.jpg";
+        }
+
+        public clsCarta()
+        {
+            this.nombre = "porDefecto";
+            this.foto = "//Assets/Images/logoSDLT.jpg";
+        }
+        #endregion
+
+        #region propiedades públicas
+        public string Nombre { get => nombre;}
+        public string Foto { get => foto; set => foto = value; }
+        #endregion
+    }
+}
