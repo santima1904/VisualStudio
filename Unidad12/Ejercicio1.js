0087aec30bbca1c51b05ded634c1897444f79aa1
Santi <santima1904@gmail.com>
2022-01-12 19:28:13 +0100

    viva tu

---
+++ Unidad12/Ejercicio1.js
@@ -0,0 +1,11 @@
+﻿window.onload = inicializaEventos;
+
+function inicializaEventos() {
+    document.getElementById("btn").addEventListener("click", saludar, false);
+}
+
+function saludar() {
+
+    alert("Hello World");
+
+}
