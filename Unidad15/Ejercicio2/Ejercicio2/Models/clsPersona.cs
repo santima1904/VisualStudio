87b5115b7dee8d9ab6bc37af0e183ae7ca1f7611
Santi <santima1904@gmail.com>
2022-01-16 18:47:32 +0100

    xamarin

---
+++ Unidad15/Ejercicio2/Ejercicio2/Models/clsPersona.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio2.Models
+{
+    public class clsPersona
+    {
+        string nombre;
+        string apellidos;
+
+        public clsPersona(string nombre, string apellidos)
+        {
+            this.nombre = nombre;
+            this.apellidos = apellidos;
+        }
+
+        public clsPersona()
+        {
+            this.nombre = "Santi";
+            this.apellidos = "Martinez";
+        }
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellidos { get => apellidos; set => apellidos = value; }
+    }
+}
