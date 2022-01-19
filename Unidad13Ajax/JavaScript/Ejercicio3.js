c1277f3055fb45aea88f3e72ced9fd058881536d
Santi <santima1904@gmail.com>
2022-01-19 19:32:24 +0100

    falta el ultimo

---
+++ Unidad13Ajax/JavaScript/Ejercicio3.js
@@ -0,0 +1,16 @@
+﻿window.onload = InicializaEventos;
+
+function InicializaEventos() {
+    document.getElementById("btnborrar").addEventListener("click", borrar, false)
+}
+
+function borrar() {
+    var miLlamada = new XMLHttpRequest();
+    miLlamada.open("DELETE", "https://crudpersonasaspsantimartinez.azurewebsites.net/api/Personas/" + document.getElementById("boxId").nodeValue);
+    //miLlamada.onreadystatechange = function () {
+    //    if (miLlamada.readyState == 4 && miLlamada.status == 200) {
+    //        document.getElementById("divapellido").innerHTML = arraypersonas[0].apellidos;
+    //    }
+    //}
+    miLlamada.send();
+}
