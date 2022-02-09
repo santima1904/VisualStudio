6c92e8a5e223dd86bca0ce1c848603986e6db105
Santi <santima1904@gmail.com>
2022-02-09 18:05:07 +0100

    patatatatatataa

---
+++ ExamenSGEMP_2T/ExamenSGEMP_2T/Controlers/PlantaController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ExamenSGEMP_2T.Controlers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlantaController : ControllerBase
+    {
+        // GET: api/<PlantaController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<PlantaController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<PlantaController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<PlantaController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<PlantaController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
